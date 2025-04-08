d651b26cc6c77552e6117c10c5d6fa396bcc730a
maxitect <maxime.downe@gmail.com>
2025-04-08 21:43:48 +0100

    refactor: apply PEP 8 standards to data_loader.py

--- model_service/src/data_loader.py
+++ model_service/src/data_loader.py
@@ -1,24 +1,43 @@
-import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
+
 def get_data_loaders(batch_size=64, download=True):
     """
-    Downloads the MNIST dataset and creates data loaders for training and testing.
-    Applies normalisation and necessary transformations.
+    Downloads the MNIST dataset and creates data loaders for training and
+    testing. Applies normalisation and necessary transformations.
     """
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))  # Mean and std for MNIST
     ])
 
-    train_dataset = datasets.MNIST(root='./data', train=True, download=download, transform=transform)
-    test_dataset = datasets.MNIST(root='./data', train=False, download=download, transform=transform)
+    train_dataset = datasets.MNIST(
+        root='./data',
+        train=True,
+        download=download,
+        transform=transform
+        )
+    test_dataset = datasets.MNIST(
+        root='./data',
+        train=False,
+        download=download,
+        transform=transform
+        )
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True
+        )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False
+        )
 
     return train_loader, test_loader
+
 
 if __name__ == "__main__":
     train_loader, test_loader = get_data_loaders()
