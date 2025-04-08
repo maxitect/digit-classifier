19100ada7a9ae7844609a59f6b6e7343a2a78d07
maxitect <maxime.downe@gmail.com>
2025-04-08 22:36:06 +0100

    refactor: format export.py with PEP 8 standards

--- model_service/src/export.py
+++ model_service/src/export.py
@@ -3,6 +3,7 @@ from torchvision import transforms
 import argparse
 from model import MNISTCNN
 
+
 def preprocess_input(image):
     """
     Preprocess a raw PIL image into a normalized tensor suitable for inference.
@@ -14,9 +15,11 @@ def preprocess_input(image):
     # Add a batch dimension
     return transform(image).unsqueeze(0)
 
+
 def export_model(model_checkpoint, export_path, device):
     """
-    Loads a trained model checkpoint, converts it to TorchScript, and exports it.
+    Loads a trained model checkpoint,
+    converts it to TorchScript, and exports it.
     """
     model = MNISTCNN().to(device)
     model.load_state_dict(torch.load(model_checkpoint, map_location=device))
@@ -25,9 +28,12 @@ def export_model(model_checkpoint, export_path, device):
     scripted_model = torch.jit.script(model)
     scripted_model.save(export_path)
     print(f"Model exported to {export_path}")
-    
+
+
 def main():
-    parser = argparse.ArgumentParser(description="Export MNIST CNN model for inference")
+    parser = argparse.ArgumentParser(
+        description="Export MNIST CNN model for inference"
+    )
     parser.add_argument(
         '--checkpoint',
         type=str,
@@ -41,9 +47,10 @@ def main():
         help='Path to save the exported model'
     )
     args = parser.parse_args()
-    
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     export_model(args.checkpoint, args.export_path, device)
+
 
 if __name__ == "__main__":
     main()
