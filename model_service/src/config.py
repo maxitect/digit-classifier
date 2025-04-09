2daa48d2fb8a69335f7174f4354db73a9bc09eed
maxitect <maxime.downe@gmail.com>
2025-04-09 11:05:24 +0100

    refactor: apply PEP 8 standards to model service config.py

--- model_service/src/config.py
+++ model_service/src/config.py
@@ -1,9 +1,11 @@
 from pydantic import BaseSettings
 
+
 class Settings(BaseSettings):
     model_path: str = "model_service/model/checkpoint.pth"
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
