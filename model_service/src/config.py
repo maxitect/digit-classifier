2a570b339e428e1285988e08144993888d2070d3
maxitect <maxime.downe@gmail.com>
2025-04-09 23:03:32 +0100

    fix: add updated exported model path to config.py

--- model_service/src/config.py
+++ model_service/src/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    model_path: str = "model_service/model/checkpoint.pth"
+    model_path: str = "model_service/src/exported_model.pt"
 
     class Config:
         env_file = ".env"
