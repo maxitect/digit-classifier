f6dcc1a3a26338f1cf827fac953912ab4d6864c8
maxitect <maxime.downe@gmail.com>
2025-04-13 23:53:59 +0100

    build: add env variables for model service export paths in config.py and docker compose

--- model_service/src/config.py
+++ model_service/src/config.py
@@ -1,8 +1,14 @@
+import os
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    model_path: str = "model_service/src/exported_model.pt"
+    checkpoints_dir: str = os.environ.get(
+        "CHECKPOINTS_DIR", "src/checkpoints/")
+    model_path: str = os.environ.get(
+        "MODEL_PATH", "model_service/src/best_model.pth")
+    model_path: str = os.environ.get(
+        "EXPORT_MODEL_PATH", "src/exported_model.pt")
 
     class Config:
         env_file = ".env"
